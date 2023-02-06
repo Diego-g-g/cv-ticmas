06806f9b7b9315d17ae69b455fca73fc3b02e65c
Diego Gauna <diego_g_pc@hotmail.com>
2023-02-06 01:53:21 -0300

    se configuro por completo el index y css de inicio

--- cv/js/index.js
+++ cv/js/index.js
@@ -5,18 +5,27 @@
 const randomUsers = async () => {
 const response = await fetch("https://randomuser.me/api/?format=JSON _");
 const user= await response.json();
-console.log(user);
+/*console.log(user);*/
+
 user["results"][0]["gender"];
 
 const nombre = user["results"][0]["name"].first;
-const genero = user["results"][0]["gender"];
+const apellido = user["results"][0]["name"].last;
+const imagen = user["results"][0]["picture"].large;
 
-document.getElementById("sexo").innerHTML += genero;
-document.getElementById("nombre").innerHTML += nombre;
+/*document.getElementById("nombre").innerHTML += nombre;*/
+var nombreCompleto = nombre+" "+apellido;
+console.log(nombreCompleto);
+document.getElementById("nomApe").innerHTML += nombreCompleto;
+
+var img = document.createElement('img');
+img.src = imagen;
+
+var foto = document.querySelector('#imagen');
+foto.appendChild(img);
+
 
 }
-
-
 window.addEventListener("load" , function () {
     randomUsers();
 });
